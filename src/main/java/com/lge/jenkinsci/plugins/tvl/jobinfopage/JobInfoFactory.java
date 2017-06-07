050357edc48573295896c1a02967b7c24cd6328b
Sunjoo Park <all4dich@gmail.com>
2017-06-07 17:43:45 +0900

    jobinfo: Enabled 'download' for Verify
    When clicking 'SUCCESS' box on a list, a developer go to a download site.
    In order to support this function, a manager has to configure
    Jenkins Url and File Archive site url on JENKINS_URL/configure.

--- src/main/java/com/lge/jenkinsci/plugins/tvl/jobinfopage/JobInfoFactory.java
+++ src/main/java/com/lge/jenkinsci/plugins/tvl/jobinfopage/JobInfoFactory.java
@@ -2,6 +2,8 @@ package com.lge.jenkinsci.plugins.tvl.jobinfopage;
 
 import hudson.Extension;
 import hudson.model.*;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.StaplerRequest;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -10,13 +12,56 @@ import java.util.Collections;
  * Created by sunjoo on 07/06/2017.
  */
 @Extension
-public class JobInfoFactory extends TransientProjectActionFactory {
-    /*
+public class JobInfoFactory extends TransientProjectActionFactory implements Describable<JobInfoFactory> {
     @Override
     public Descriptor<JobInfoFactory> getDescriptor() {
         return null;
     }
-    */
+
+    @Extension
+    public static final DescriptorForJobInfoFactory DESCRIPTOR = new DescriptorForJobInfoFactory();
+
+    public static class DescriptorForJobInfoFactory extends Descriptor<JobInfoFactory>{
+        private String tvlArchiveRootUrl;
+        private String svlArchiveRootUrl;
+        private String tvlJenkinsUrl;
+        private String svlJenkinsUrl;
+
+        public DescriptorForJobInfoFactory(){
+            load();
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException{
+            tvlArchiveRootUrl = formData.getString("tvlArchiveRootUrl");
+            svlArchiveRootUrl = formData.getString("svlArchiveRootUrl");
+            tvlJenkinsUrl = formData.getString("tvlJenkinsUrl");
+            svlJenkinsUrl = formData.getString("svlJenkinsUrl");
+            save();
+            return super.configure(req, formData);
+        }
+
+        public String getTvlArchiveRootUrl(){
+            return tvlArchiveRootUrl;
+        }
+
+        public String getSvlArchiveRootUrl(){
+            return svlArchiveRootUrl;
+        }
+
+        public String getTvlJenkinsUrl(){
+            return tvlJenkinsUrl;
+        }
+
+        public String getSvlJenkinsUrl(){
+            return svlJenkinsUrl;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Job Information Display";
+        }
+    }
 
     @Override
     public Collection<? extends Action> createFor(AbstractProject target) {
--- src/main/java/com/lge/jenkinsci/plugins/tvl/jobinfopage/VerifyJobInfo.java
+++ src/main/java/com/lge/jenkinsci/plugins/tvl/jobinfopage/VerifyJobInfo.java
@@ -2,9 +2,12 @@ package com.lge.jenkinsci.plugins.tvl.jobinfopage;
 
 import hudson.model.*;
 import hudson.util.RunList;
+import jenkins.model.Jenkins;
 
 import java.lang.reflect.Parameter;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.*;
 
 /**
@@ -36,12 +39,25 @@ public class VerifyJobInfo extends InvisibleAction {
     }
 
     public List<Map<String, String>> getData(){
+        String svlArchiveRootUrl = JobInfoFactory.DESCRIPTOR.getSvlArchiveRootUrl();
+        String tvlArchiveRootUrl = JobInfoFactory.DESCRIPTOR.getTvlArchiveRootUrl();
+        String svlJenkinsUrl = JobInfoFactory.DESCRIPTOR.getSvlJenkinsUrl();
+        String tvlJenkinsUrl = JobInfoFactory.DESCRIPTOR.getTvlJenkinsUrl();
+        String archiveRootUrl = "";
+        Jenkins instance = Jenkins.getInstance();
+        String rootUrl = instance.getRootUrl();
+        if (rootUrl.equals(tvlJenkinsUrl)){
+            archiveRootUrl = tvlArchiveRootUrl + "starfish_verifications/";
+        }else  if(rootUrl.equals(svlJenkinsUrl)){
+            archiveRootUrl = svlArchiveRootUrl;
+        }
         String name = project.getName();
         RunList r = project.getBuilds();
         Iterator i = r.iterator();
         List<Map<String, String>> list = new ArrayList<>();
         String dateFormat = "yyyy-MM-dd hh:mm a";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
+        LocalDateTime limitDay = LocalDateTime.now().minusDays(7);
         while(i.hasNext()){
             Run each_run = (Run) i.next();
             List<Action> actions = (List<Action>) each_run.getAllActions();
@@ -55,17 +71,21 @@ public class VerifyJobInfo extends InvisibleAction {
             String description = each_run.getDescription();
             each_r.put("description", description);
             long startTime = each_run.getStartTimeInMillis();
+            if ( startTime < limitDay.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()){
+                continue;
+            }
             Date startDate = new Date(startTime);
             each_r.put("when", simpleDateFormat.format(startDate.getTime()));
             long duration = each_run.getDuration();
             each_r.put("duration", String.valueOf(duration));
             long estimataedDuration = each_run.getEstimatedDuration();
-
             each_r.put("gerrit_project", parameterAction.getParameter("GERRIT_PROJECT").getValue().toString());
             each_r.put("gerrit_change_url", parameterAction.getParameter("GERRIT_CHANGE_URL").getValue().toString());
             each_r.put("gerrit_change_owner_name", parameterAction.getParameter("GERRIT_CHANGE_OWNER_NAME").getValue().toString());
             each_r.put("gerrit_change_number", parameterAction.getParameter("GERRIT_CHANGE_NUMBER").getValue().toString());
             each_r.put("gerrit_patchset_number", parameterAction.getParameter("GERRIT_PATCHSET_NUMBER").getValue().toString());
+            each_r.put("url", rootUrl + each_run.getUrl());
+            each_r.put("download_url", archiveRootUrl + name + "/" + String.valueOf(number));
             list.add(each_r);
         }
         return list;
